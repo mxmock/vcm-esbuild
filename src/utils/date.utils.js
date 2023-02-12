25fb5a5ba3ab43680eb75dcb2862f2dfb57ec9c0
Max <oc.kode@gmail.com>
2023-02-12 19:03:47 +0100

    date utils

---
+++ src/utils/date.utils.js
@@ -0,0 +1,12 @@
+const getToday = () => {
+  const date = new Date();
+  return [date.getFullYear(), date.getMonth() + 1, date.getDate()].reduce(
+    (toBuild, val, index) => {
+      const value = val < 10 ? `0${val}` : `${val}`;
+      return (toBuild += !index ? value : `-${value}`);
+    },
+    ""
+  );
+};
+
+export default getToday;
