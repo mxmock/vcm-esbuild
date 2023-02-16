63531cb8e53dbe828be006cb6c8d6a65bc8e0bad
Max <oc.kode@gmail.com>
2023-02-16 09:48:57 +0100

    Card component

---
+++ src/components/Card.jsx
@@ -0,0 +1,46 @@
+import Icon from "./Icon";
+import React, { useRef } from "react";
+
+const Card = ({ title, children }) => {
+  const cardBody = useRef(null);
+
+  const handleExtend = (extend) => {
+    const height = extend ? 200 : cardBody.current.scrollHeight + 32;
+    setCardHeight(`${height}px`);
+    setExtended(!extend);
+  };
+
+  const [extended, setExtended] = React.useState(false);
+  const [cardHeight, setCardHeight] = React.useState("200px");
+
+  return (
+    <div className={`card ${extended ? "card--extended" : ""}`}>
+      <div className="card__title">
+        <h2>{title}</h2>
+      </div>
+
+      <div
+        ref={cardBody}
+        className="card__body"
+        style={{ ["--card-body-height"]: `${cardHeight}` }}
+      >
+        {children}
+      </div>
+
+      <div className="card__read-more">
+        <button onClick={() => handleExtend(extended)}>
+          <span>{extended ? "voir moins" : "voir plus"}</span>
+          <Icon
+            name={
+              extended
+                ? "arrow-ios-upward-outline"
+                : "arrow-ios-downward-outline"
+            }
+          />
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
