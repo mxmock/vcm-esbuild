fdc4140554a0004a2b010ec9f26e864473e6a1a1
mxmock <oc.kode@gmail.com>
2023-02-28 15:13:26 +0100

    Fix villa details render

--- src/components/VillaDetails.static.jsx
+++ src/components/VillaDetails.static.jsx
@@ -5,6 +5,197 @@ import Feature from "./Feature";
 import villasData from "../data/villas.data.json";
 import annexeData from "../data/annexe.data.json";
 
+// const initFeatures = {
+//   rooms: [],
+//   stuff: [],
+//   around: [],
+//   services: [],
+//   globalView: [],
+//   additionnals: [],
+//   mainFeatures: [],
+// };
+
+const VillaDetails = ({ data }) => {
+  const villa = villasData.find((v) => v.id === data.id);
+
+  const rooms = [...villa.rooms, annexeData].map(mapRoom);
+  const stuff = villa.features.filter((f) => byCat(f, 2)).map(mapFeature);
+  const around = villa.features.filter((f) => byCat(f, 5)).map(mapFeature);
+  const services = villa.features.filter((f) => byCat(f, 3)).map(mapFeature);
+  const globalView = villa.features.filter((f) => byCat(f, 1)).map(mapFeature);
+  const mainFeatures = villa.features.filter((f) => byCat(f, 6)).map(mapFeature);
+  const additionnals = villa.features.filter((f) => byCat(f, 4)).map(mapFeature);
+
+  // const [villa, setVilla] = React.useState(null);
+  // const [{ rooms, stuff, around, services, globalView, mainFeatures, additionnals }, setFeatures] =
+  //   React.useState(initFeatures);
+
+  // React.useEffect(() => {
+  //   if (!villasData || !data?.id) return;
+  //   const villa = villasData.find((v) => v.id === data.id);
+  //   setVilla(villa);
+  //   setFeatures({
+  //     rooms: [...villa.rooms, annexeData].map(mapRoom),
+  //     stuff: villa.features.filter((f) => byCat(f, 2)).map(mapFeature),
+  //     around: villa.features.filter((f) => byCat(f, 5)).map(mapFeature),
+  //     services: villa.features.filter((f) => byCat(f, 3)).map(mapFeature),
+  //     globalView: villa.features.filter((f) => byCat(f, 1)).map(mapFeature),
+  //     mainFeatures: villa.features.filter((f) => byCat(f, 6)).map(mapFeature),
+  //     additionnals: villa.features.filter((f) => byCat(f, 4)).map(mapFeature),
+  //   });
+
+  //   console.log(villa);
+  // }, [villasData, annexeData, data]);
+
+  return (
+    <div className="villa-details">
+      <ul className="villa-details__main-features">
+        {mainFeatures.map((f) => (
+          <li key={f.id}>
+            <Feature feature={f} />
+          </li>
+        ))}
+      </ul>
+
+      <Card
+        readMore="lire la suite"
+        title={"Description"}
+      >
+        <div
+          className="villa-details__text"
+          dangerouslySetInnerHTML={{ __html: villa.descriptionFR }}
+        ></div>
+      </Card>
+
+      <Card title="Vue d'ensemble">
+        <ul className="villa-details__columns">
+          {globalView.map((f) => (
+            <li key={f.id}>
+              <div className="villa-details__second-features">
+                <Icon name="sun-outline" />
+                <div>
+                  <span>{f.labelFR}</span>
+                  {f.value && (
+                    <span>
+                      : {f.value} (dont {f.value} privatifs)
+                    </span>
+                  )}
+                </div>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card
+        readMore="voir tous les équipements"
+        title={"Équipement"}
+      >
+        <ul className="villa-details__columns">
+          {stuff.map((f) => (
+            <li key={f.id}>
+              <div className="villa-details__second-features">
+                <Icon name="sun-outline" />
+                <span>{f.labelFR}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card title="Informations sur les chambres">
+        <ul className="villa-details__columns">
+          {rooms.map((r) => (
+            <li
+              key={r.id}
+              className="villa-details__room"
+            >
+              <h4>{r.label ? r.label : "Chambre classique"}</h4>
+              <ul>
+                {r.beds.map((b, i) => (
+                  <li
+                    key={i}
+                    className="villa-details__second-features"
+                  >
+                    <Icon name="bed-simple-outline" />
+                    <span>Lit {b}</span>
+                  </li>
+                ))}
+              </ul>
+              <div className="villa-details__second-features">
+                {r.convertible && (
+                  <>
+                    <Icon name="sofa-outline" />
+                    <span>Canapé convertible</span>
+                  </>
+                )}
+              </div>
+              <div className="villa-details__second-features">
+                {r.bathroom && (
+                  <>
+                    <Icon name="bath-outline" />
+                    <span>Salle de bain</span>
+                  </>
+                )}
+              </div>
+              <div className="villa-details__second-features">
+                {r.wc && (
+                  <>
+                    <Icon name="wc-outline" />
+                    <span>WC privatif</span>
+                  </>
+                )}
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card
+        readMore="voir tous les services"
+        title={"Services inclus"}
+      >
+        <ul className="villa-details__columns">
+          {services.map((f) => (
+            <li key={f.id}>
+              <div className="villa-details__second-features">
+                <Icon name="sun-outline" />
+                <span>{f.labelFR}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card title={"Services supplémentaires"}>
+        <ul className="villa-details__columns">
+          {additionnals.map((f) => (
+            <li key={f.id}>
+              <div className="villa-details__second-features">
+                <Icon name="sun-outline" />
+                <span>{f.labelFR}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+
+      <Card title={"Aux alentours"}>
+        <ul className="villa-details__columns">
+          {around.map((f) => (
+            <li key={f.id}>
+              <div className="villa-details__second-features">
+                <Icon name="sun-outline" />
+                <span>{f.labelFR}</span>
+              </div>
+            </li>
+          ))}
+        </ul>
+      </Card>
+    </div>
+  );
+};
+
 const byCat = (f, category) => f.category === category;
 
 const mapFeature = (f) => ({
@@ -28,191 +219,6 @@ const mapRoom = (room) => {
     pictures: room.documents,
     convertible: room.convertible,
   };
-};
-
-const initFeatures = {
-  rooms: [],
-  stuff: [],
-  around: [],
-  services: [],
-  globalView: [],
-  additionnals: [],
-  mainFeatures: [],
-};
-
-const VillaDetails = ({ data }) => {
-  const [villa, setVilla] = React.useState(null);
-  const [{ rooms, stuff, around, services, globalView, mainFeatures, additionnals }, setFeatures] =
-    React.useState(initFeatures);
-
-  React.useEffect(() => {
-    if (!villasData || !data?.id) return;
-    const villa = villasData.find((v) => v.id === data.id);
-    setVilla(villa);
-    setFeatures({
-      rooms: [...villa.rooms, annexeData].map(mapRoom),
-      stuff: villa.features.filter((f) => byCat(f, 2)).map(mapFeature),
-      around: villa.features.filter((f) => byCat(f, 5)).map(mapFeature),
-      services: villa.features.filter((f) => byCat(f, 3)).map(mapFeature),
-      globalView: villa.features.filter((f) => byCat(f, 1)).map(mapFeature),
-      mainFeatures: villa.features.filter((f) => byCat(f, 6)).map(mapFeature),
-      additionnals: villa.features.filter((f) => byCat(f, 4)).map(mapFeature),
-    });
-
-    console.log(villa);
-  }, [villasData, annexeData, data]);
-
-  return (
-    <>
-      {villa && (
-        <div className="villa-details">
-          <ul className="villa-details__main-features">
-            {mainFeatures.map((f) => (
-              <li key={f.id}>
-                <Feature feature={f} />
-              </li>
-            ))}
-          </ul>
-
-          <Card
-            readMore="lire la suite"
-            title={"Description"}
-          >
-            <div
-              className="villa-details__text"
-              dangerouslySetInnerHTML={{ __html: villa.descriptionFR }}
-            ></div>
-          </Card>
-
-          <Card title="Vue d'ensemble">
-            <ul className="villa-details__columns">
-              {globalView.map((f) => (
-                <li key={f.id}>
-                  <div className="villa-details__second-features">
-                    <Icon name="sun-outline" />
-                    <div>
-                      <span>{f.labelFR}</span>
-                      {f.value && (
-                        <span>
-                          : {f.value} (dont {f.value} privatifs)
-                        </span>
-                      )}
-                    </div>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-
-          <Card
-            readMore="voir tous les équipements"
-            title={"Équipement"}
-          >
-            <ul className="villa-details__columns">
-              {stuff.map((f) => (
-                <li key={f.id}>
-                  <div className="villa-details__second-features">
-                    <Icon name="sun-outline" />
-                    <span>{f.labelFR}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-
-          <Card title="Informations sur les chambres">
-            <ul className="villa-details__columns">
-              {rooms.map((r) => (
-                <li
-                  key={r.id}
-                  className="villa-details__room"
-                >
-                  <h4>{r.label ? r.label : "Chambre classique"}</h4>
-                  <ul>
-                    {r.beds.map((b, i) => (
-                      <li
-                        key={i}
-                        className="villa-details__second-features"
-                      >
-                        <Icon name="bed-simple-outline" />
-                        <span>Lit {b}</span>
-                      </li>
-                    ))}
-                  </ul>
-                  <div className="villa-details__second-features">
-                    {r.convertible && (
-                      <>
-                        <Icon name="sofa-outline" />
-                        <span>Canapé convertible</span>
-                      </>
-                    )}
-                  </div>
-                  <div className="villa-details__second-features">
-                    {r.bathroom && (
-                      <>
-                        <Icon name="bath-outline" />
-                        <span>Salle de bain</span>
-                      </>
-                    )}
-                  </div>
-                  <div className="villa-details__second-features">
-                    {r.wc && (
-                      <>
-                        <Icon name="wc-outline" />
-                        <span>WC privatif</span>
-                      </>
-                    )}
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-
-          <Card
-            readMore="voir tous les services"
-            title={"Services inclus"}
-          >
-            <ul className="villa-details__columns">
-              {services.map((f) => (
-                <li key={f.id}>
-                  <div className="villa-details__second-features">
-                    <Icon name="sun-outline" />
-                    <span>{f.labelFR}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-
-          <Card title={"Services supplémentaires"}>
-            <ul className="villa-details__columns">
-              {additionnals.map((f) => (
-                <li key={f.id}>
-                  <div className="villa-details__second-features">
-                    <Icon name="sun-outline" />
-                    <span>{f.labelFR}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-
-          <Card title={"Aux alentours"}>
-            <ul className="villa-details__columns">
-              {around.map((f) => (
-                <li key={f.id}>
-                  <div className="villa-details__second-features">
-                    <Icon name="sun-outline" />
-                    <span>{f.labelFR}</span>
-                  </div>
-                </li>
-              ))}
-            </ul>
-          </Card>
-        </div>
-      )}
-    </>
-  );
 };
 
 export default VillaDetails;
