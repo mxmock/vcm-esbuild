18e0ee96b90301e494538d34bde3b5b71c884bd2
mxmock <oc.kode@gmail.com>
2023-02-28 16:24:20 +0100

    Fix blinking modal

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -6,33 +6,37 @@ import { useKeyPress } from "../hooks/keypress.hook";
 const Modal = (props) => {
   const { children, position, fullscreen, onClose } = props;
 
-  const [modalPosition, setModalPosition] = React.useState({ x: `50%`, y: `50%` });
+  const [modalPosition, setModalPosition] = React.useState(null);
 
   useKeyPress("Escape", onClose);
 
   React.useEffect(() => {
-    if (!children || !position) return;
-    const y = `calc(${position.y}px - var(--modal-padding))`;
-    const x = `calc(50% + ${position.x}px - var(--100vw) / 2)`;
+    if (!children) return;
+    const y = position ? `calc(${position.y}px - var(--modal-padding))` : `50%`;
+    const x = position ? `calc(50% + ${position.x}px - var(--100vw) / 2)` : `50%`;
     setModalPosition({ x, y });
   }, [children, position]);
 
   const modalHtml = (
-    <div className={`modal ${fullscreen ? "modal--fullscreen" : ""}`}>
-      <FocusTrapper>
-        <div
-          role="dialog"
-          aria-modal={true}
-          className="modal__card"
-          style={{
-            ["--top-origin"]: `${modalPosition.y}`,
-            ["--left-origin"]: `${modalPosition.x}`,
-          }}
-        >
-          {children}
+    <>
+      {!!modalPosition && (
+        <div className={`modal ${fullscreen ? "modal--fullscreen" : ""}`}>
+          <FocusTrapper>
+            <div
+              role="dialog"
+              aria-modal={true}
+              className="modal__card"
+              style={{
+                ["--top-origin"]: `${modalPosition.y}`,
+                ["--left-origin"]: `${modalPosition.x}`,
+              }}
+            >
+              {children}
+            </div>
+          </FocusTrapper>
         </div>
-      </FocusTrapper>
-    </div>
+      )}
+    </>
   );
 
   return (
