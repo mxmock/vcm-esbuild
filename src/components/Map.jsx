df3b47708d23f98313474cd1b3d470b35796bc03
mxmock <oc.kode@gmail.com>
2023-03-05 16:23:45 +0100

    Merge branch 'dev' into 'feat/booking-form'

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import * as L from "leaflet";
 
 // const TOKEN_MAPBOX = "pk.eyJ1Ijoid2lzMjM5IiwiYSI6ImNrOHgwZjA2ZzB2cmgzam83emxiZHNyMWsifQ.T56-iKyo0HADQmC1LKyhag";
 
@@ -8,14 +7,14 @@ const MAP_URL = "https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png";
 
 const MAP_ID = `mapid`;
 
-const MAP_CONFIG = {
+const getMapConfig = (L) => ({
   zoom: 10,
   minZoom: 1,
   maxZoom: 17,
   scrollWheelZoom: false,
   attributionControl: false,
   center: new L.LatLng(16.253116, -61.576572), // centré sur Baie-Mahault
-};
+});
 
 // const LAYER_CONFIG = {
 //   maxZoom: 20,
@@ -25,18 +24,19 @@ const MAP_CONFIG = {
 //   accessToken: TOKEN_MAPBOX,
 // };
 
-const MARKER_ICON_CONFIG = {
-  iconUrl: "../../assets/images/villa-marker.png",
+const getMarkerConfig = (marker) => ({
+  iconUrl: marker,
   iconSize: [35, 45], // size of the icon
   iconAnchor: [18, 47], // point of the icon which will correspond to marker's location
   popupAnchor: [0, -40], // point from which the popup should open relative to the iconAnchor
-};
+});
 
 const Map = ({ name, villaLocation }) => {
-  const initMap = () => {
-    if (!!map) return;
+  const initMap = async () => {
+    if (typeof window === "undefined" || !!map) return;
+    const L = await import("leaflet");
     // load map config
-    setMap(L.map(`${MAP_ID}`, { ...MAP_CONFIG }));
+    setMap(L.map(`${MAP_ID}`, { ...getMapConfig(L) }));
     // set villa location
     setLocation(new L.LatLng(villaLocation.latitude, villaLocation.longitude));
   };
@@ -51,8 +51,10 @@ const Map = ({ name, villaLocation }) => {
     addMarker();
   };
 
-  const addMarker = () => {
-    const icon = L.icon({ ...MARKER_ICON_CONFIG });
+  const addMarker = async () => {
+    if (typeof window === "undefined") return;
+    const marker = await import("../assets/images/villa-marker.png");
+    const icon = L.icon({ ...getMarkerConfig(marker.default) });
     const villaMarker = L.marker(location, { icon }).addTo(map);
     villaMarker.bindPopup(`${name}`).openPopup();
   };
@@ -60,7 +62,9 @@ const Map = ({ name, villaLocation }) => {
   const [map, setMap] = React.useState(null);
   const [location, setLocation] = React.useState(null);
 
-  React.useEffect(initMap, []);
+  React.useEffect(() => {
+    initMap();
+  }, []);
   React.useEffect(configureLayer, [map, location]);
 
   return (
